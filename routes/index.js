49d3ce94b69d414eae12225e9423c9113a3f06d2
Mason Kolehmainen <mason.kolehmainen@gmail.com>
2022-10-14 10:11:33 -0400

    Dynamically serve the sitemap xml file

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const path = require('path');
 const router = require('express').Router();
 
 router.use(require(path.join(process.cwd(), 'routes', '/images.js')));
+router.use('/sitemap.xml', require(path.join(process.cwd(), 'routes', '/sitemap.js')));
 router.use(require(path.join(process.cwd(), 'routes', '/pages.js')));
 
 module.exports = router;
---
+++ routes/sitemap.js
@@ -0,0 +1,39 @@
+const builder = require('xmlbuilder');
+const fs = require('fs');
+const path = require('path');
+const router = require('express').Router();
+
+router.get('/', (req, res) => {
+	let organizations = fs.readdirSync(path.join(process.cwd(), 'assets/organizations'))
+		.filter(x => x.endsWith('.json'))
+		.map(x => { return {
+			href: `https://masons.photography/${x.split('.')[0].replace('_', '-')}/`,
+			modified: fs.statSync(path.join(process.cwd(), 'assets/organizations', x))['mtime'].toISOString()
+		}});
+
+	let events = fs.readdirSync(path.join(process.cwd(), 'assets/events'))
+		.filter(x => x.endsWith('.json'))
+		.map(x => JSON.parse(fs.readFileSync(path.join(process.cwd(), 'assets/events', x))))
+		.filter(x => !x.href)
+		.map(x => { return {
+			href: `https://masons.photography/${x.organization.toLowerCase().replace(' ', '-')}/${x.name.toLowerCase().replace(' ', '-')}/`,
+			modified: fs.statSync(path.join(process.cwd(), 'assets/events', `${x.name.toLowerCase().replace(' ', '_')}.json`))['mtime'].toISOString()
+		}});
+
+	let pages = [{
+		href: 'https://masons.photography/',
+		modified: fs.statSync(path.join(process.cwd(), 'index.html'))['mtime'].toISOString()
+	}].concat(organizations, events);
+
+	let xml = builder.create('urlset', { version: '1.0', encoding: 'UTF-8' }, null, { noValidation: true });
+	for (let { href, modified } of pages) {
+		let url = xml.element('url');
+		url.element('loc', href);
+		url.element('lastmod', modified);
+		url.element('priority', (1.0 - (((href.match(/\//g) || []).length - 3) * 0.2)).toFixed(1));
+	}
+
+	res.type('xml').status(200).send(xml.end({ pretty: true}));
+});
+
+module.exports = router;
